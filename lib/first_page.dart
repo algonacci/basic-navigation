77ebbdaa7965f2b34669081e505a20a596a1eda3
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2022-12-25 18:29:21 +0700

    learnt WillPopScope

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -8,38 +8,46 @@ class FirstPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text(
-          'First Page',
+    return WillPopScope(
+      onWillPop: () {
+        debugPrint('WillPopScope will run');
+        number = Random().nextInt(100);
+        Navigator.pop(context, number);
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text(
+            'First Page',
+          ),
+          backgroundColor: Colors.red,
         ),
-        backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'First Page',
-              style: TextStyle(
-                fontSize: 36,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'First Page',
+                style: TextStyle(
+                  fontSize: 36,
+                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                number = Random().nextInt(100);
-                debugPrint('=== Number $number ===');
-                Navigator.of(context).pop(number);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
+              ElevatedButton(
+                onPressed: () {
+                  number = Random().nextInt(100);
+                  debugPrint('=== Number $number ===');
+                  Navigator.of(context).pop(number);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                child: const Text(
+                  'Back Button',
+                ),
               ),
-              child: const Text(
-                'Back Button',
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
